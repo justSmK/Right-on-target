c6ecef72e70fccf9be404e1149676e3eed193f24
Sergei Semko <semko431@gmail.com>
2022-02-16 12:36:59 +0300

    Scene change option

---
+++ Right on target/SecondViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SecondViewController.swift
+//  Right on target
+//
+//  Created by justSmK on 16.02.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad SecondViewController")
+        // Do any additional setup after loading the view.
+    }
+    
+    override func loadView() {
+        super.loadView()
+        print("loadView SecondViewController")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear SecondViewController")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear SecondViewController")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear SecondViewController")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear SecondViewController")
+    }
+
+    @IBAction func hideCurrentScene() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
--- Right on target/ViewController.swift
+++ Right on target/ViewController.swift
@@ -13,47 +13,80 @@ class ViewController: UIViewController {
     @IBOutlet var label: UILabel!
     
     var number: Int = 0 //hidden number
-    var round: Int = 0 //round num
+    var round: Int = 1 //round num
     var points: Int = 0 //sum of points per round
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad")
+        
+        self.number = Int.random(in: 1...50)
+        self.label.text = String(self.number)
+    }
+    
+    override func loadView() {
+        super.loadView()
+        print("loadView")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
     }
     
     @IBAction func checkNumber() {
-        if self.round == 0 {
-            self.number = Int.random(in: 1...50)
-            self.label.text = String(self.number)
-            self.round = 1
+        
+        let numSlider = Int(self.slider.value.rounded())
+        if numSlider > self.number {
+            self.points += 50 - numSlider + self.number
+        } else if numSlider < self.number {
+            self.points += 50 - self.number + numSlider
         } else {
-            let numSlider = Int(self.slider.value.rounded())
-            if numSlider > self.number {
-                self.points += 50 - numSlider + self.number
-            } else if numSlider < self.number {
-                self.points += 50 - self.number + numSlider
-            } else {
-                self.points += 50
-            }
-            if self.round == 5 {
-                let alert = UIAlertController(
-                    title: "Game over",
-                    message: "You have earned \(self.points) points",
-                    preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Start over",
-                                              style: .default,
-                                              handler: nil))
-                self.present(alert, animated: true, completion: nil)
-                self.round = 1
-                self.points = 0
-            } else {
-                self.round += 1
-            }
-            self.number = Int.random(in: 1...50)
-            self.label.text = String(self.number)
+            self.points += 50
         }
+        if self.round == 5 {
+            let alert = UIAlertController(
+                title: "Game over",
+                message: "You have earned \(self.points) points",
+                preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Start over",
+                                          style: .default,
+                                          handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            self.round = 1
+            self.points = 0
+        } else {
+            self.round += 1
+        }
+        self.number = Int.random(in: 1...50)
+        self.label.text = String(self.number)
     }
 
+    lazy var secondViewController: SecondViewController = getSecondViewController()
+    private func getSecondViewController() -> SecondViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "SecondViewController")
+        return viewController as! SecondViewController
+    }
+    
+    @IBAction func showNextScreen() {
+        self.present(secondViewController, animated: true, completion: nil)
+    }
 
 }
 
